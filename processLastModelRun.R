6115cfc4e93018c00a5b2593e1268be386285e79
Bryce McCall <01425453@wf.uct.ac.za>
2017-03-14 12:27:12 +0200

    Added new file - runs extraction on the last model you ran automattically.

---
+++ processLastModelRun.R
@@ -0,0 +1,50 @@
+#RUN extract results from an input GDX file name 
+#SAVE result as rds file with same name
+#BMc. Edited this so it calls the answer model extractor. removed cmd line references
+
+library(reshape2)
+library(gdxrrw)
+library(dplyr)
+library(rpivotTable)
+library(data.table)
+library(shiny)
+
+#the GDX files location
+gdxLocation = 'C:/EMOD/GDXout/'
+saverdspath = 'C:/EMOD/RDSfiles/'
+
+fileDetails = file.info(list.files(gdxLocation,pattern= '*.gdx',full.names = T))
+fileDetails = fileDetails[with(fileDetails,order(as.POSIXct(mtime),decreasing = T)),]
+
+gdxfile = rownames(fileDetails)[1]
+gdxPath = gdxfile
+gdxname = gsub('.*/','',gdxfile)#extract the name of the gdx file without the extension
+gdxname = gsub('.gdx','',gdxname)
+
+#file paths used in extractResults
+
+# location of your GAMS main directory. 
+GAMS_lib_dir = 'C:/GAMS/win64/24.7' 
+# connect to the GAMS library.
+igdx(GAMS_lib_dir) 
+
+#main working directory for this file/script and colourcoding files etc. 
+workdir = paste(getwd(),'satimviz_backend/',sep = '/') # this is also needed for the extractResults.R script
+
+
+#LOAD FUNCTIONS
+source(paste(workdir,'extractResults.R',sep =''))
+
+print(paste('Running results extraction on: ',gdxfile,sep = ''))
+
+#PROCESS GDX FILE:
+tmplist = list()
+#setwd(gdxLocation)
+
+listname = gdxname
+tmplist[[listname]] = processGDX(gdxPath,gdxname)
+
+#SAVE RDS FILE
+rdsname = gdxname
+saveRDS(tmplist,paste(saverdspath,paste(rdsname,'.rds',sep = ''),sep = ''))
+print('results extraction saved')
